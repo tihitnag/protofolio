3a325d29b71154311d3e6a0032a5512c51212827
tihitnag <111021311+tihitnag@users.noreply.github.com>
2023-03-15 05:20:10 -0700

    Add files via upload

---
+++ tihina.js
@@ -0,0 +1,14 @@
+<div id='list'>
+  <h2>Technical Skills</h3>
+<ul ><br>
+  <li><h3>  Good command of application development in flutter/Dart and firebase</h3>  </li>
+  <li><h3>   Practical knowledge of SQL and database concepts</h3> </li>
+  <li> <h3>  Good command of web development ( Angular, .NET , HTML , CSS ,JavaScript )</h3>  </li>
+   <li> <h3>   General understanding of C++ , Python ,Java</h3>  </li>
+   <li> <h3>  Good command of version controlling with git/github</h3> </li>
+
+</ul>
+ 
+
+</div>
+<p> Angular, .NET ,flutter,java, HTML,JavaScript</p>
